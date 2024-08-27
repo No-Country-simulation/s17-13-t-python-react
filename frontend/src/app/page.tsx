c7e6e9dfd3e6018036105ac00d857e52a61cf606
juanRCoder <guillecarck@gmail.com>
2024-08-27 16:19:51 -0500

    feat: add modal in the page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Component from "@/components/Component";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      empty template
-    </main>
+    <>
+      <Component />
+    </>
   );
 }
---
+++ frontend/src/interfaces/modal.ts
@@ -0,0 +1,3 @@
+export interface propModal {
+    setOpenModal: (bool: boolean) => void;
+}
