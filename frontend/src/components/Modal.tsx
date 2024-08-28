6531408c55a8664c89be2638062e514b05306918
Lucas Clemente <lucasclemente2016@gmail.com>
2024-08-28 17:49:23 -0300

    complete SearchBar

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,11 +1,10 @@
-"use client"
+'use client';
 
-import { propModal } from "@/interfaces/modal";
-import { useState } from "react"
-import OptionModal from "./OptionModal";
-import { options } from "@/utils/modal";
-import '../styles/Modal.css'
-
+import { propModal } from '@/interfaces/modal';
+import { useState } from 'react';
+import OptionModal from './OptionModal';
+import { options } from '@/utils/modal';
+import '../styles/Modal.css';
 
 export default function Modal({ setOpenModal }: propModal) {
   const [preference, setPreference] = useState<string[]>([]);
@@ -14,54 +13,56 @@ export default function Modal({ setOpenModal }: propModal) {
   // Hace el "toggle" en cada opcion
   const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, checked } = e.target;
-    setCheckedItems((prev => ({ ...prev, [id]: checked })));
-  }
+    setCheckedItems((prev) => ({ ...prev, [id]: checked }));
+  };
 
   // Agrega los gustos dependiendo del estado de cada opcion
   const preferenceModalHandler = (text: string, status: boolean) => {
-    status 
-      ? setPreference((prev => ([...prev, text])))
-      : setPreference((prev => prev.filter(txt => txt !== text)))
-  }
+    status
+      ? setPreference((prev) => [...prev, text])
+      : setPreference((prev) => prev.filter((txt) => txt !== text));
+  };
 
   // Mostrar gustos por consola y cerrar modal
   const sendPreference = () => {
-    console.log(preference) // ESTO SERIAN LOS DATOS A ENVIAR AL BACKEND
-    setOpenModal(false)
-  }
+    console.log(preference); // ESTO SERIAN LOS DATOS A ENVIAR AL BACKEND
+    setOpenModal(false);
+  };
 
   return (
     <>
-      <section className="min-h-screen w-full sm:flex justify-center items-center absolute sm:fixed top-0 left-0 bg-black/60">
-        <div className="w-full sm:w-fit sm:flex sm:flex-col sm:items-center gap-5 relative bg-[#EEE4F7] sm:rounded-[25px] pb-4 pt-14 sm:pt-10">
-          <button className="absolute right-5 top-3 font-extrabold text-2xl text-[#49454F]"
+      <section className="absolute left-0 top-0 min-h-screen w-full items-center justify-center bg-black/60 sm:fixed sm:flex">
+        <div className="relative w-full gap-5 bg-[#EEE4F7] pb-4 pt-14 sm:flex sm:w-fit sm:flex-col sm:items-center sm:rounded-[25px] sm:pt-10">
+          <button
+            className="absolute right-5 top-3 text-2xl font-extrabold text-[#49454F]"
             onClick={() => setOpenModal(false)}
           >
             ✕
           </button>
-          <h4 className="sm:w-[434px] text-[#1E1E1E] text-xl text-center font-bold px-4 sm:px-0">
+          <h4 className="px-4 text-center text-xl font-bold text-[#1E1E1E] sm:w-[434px] sm:px-0">
             Para conocerte mejor, seleccioná algunas categorias que te gustaría ver
           </h4>
-          <section className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4 gap-y-4 sm:gap-y-10 p-5 px-4 sm:px-14">
+          <section className="grid grid-cols-1 gap-4 gap-y-4 p-5 px-4 sm:grid-cols-3 sm:gap-y-10 sm:px-14 md:grid-cols-4">
             {options.map((option, i) => (
-              <OptionModal 
+              <OptionModal
                 key={i}
-                option={option} 
-                checkedItems={checkedItems} 
-                handleCheck={handleCheck} 
+                option={option}
+                checkedItems={checkedItems}
+                handleCheck={handleCheck}
                 preferenceModalHandler={preferenceModalHandler}
               />
             ))}
           </section>
-          <div className="flex justify-end w-full px-4 sm:px-14">
+          <div className="flex w-full justify-end px-4 sm:px-14">
             <button
-              onClick={sendPreference} 
-              className="bg-[#1E1E1E] text-white w-full sm:w-[145px] h-[45px] grid place-items-center rounded-full">
+              onClick={sendPreference}
+              className="grid h-[45px] w-full place-items-center rounded-full bg-[#1E1E1E] text-white sm:w-[145px]"
+            >
               Continuar
             </button>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,60 +1,59 @@
-
 'use client';
-import React,{useState,useEffect} from 'react';
-import { FaSearch } from "react-icons/fa";
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
 import { create } from 'zustand';
 
 interface Libro {
-    firstName: string;
-    lastName: string;
-  }
-interface message {message: string};
-interface SearchId{id: number}
+  firstName: string;
+  lastName: string;
+}
+interface message {
+  message: string;
+}
+interface SearchId {
+  id: number;
+}
 
 const SearchBar = () => {
-  
-    const [libros, setLibros] = useState<Libro[]>([]); 
-    const [mensaje, setMensaje] = useState<string>(''); 
-    const [searchId, setSearchId] = useState<number | null>(null); //
+  const [libros, setLibros] = useState<Libro[]>([]);
+  const [mensaje, setMensaje] = useState<string>('');
+  const [searchId, setSearchId] = useState<number | null>(null); //
 
-    const handleSearch = () => {
-        // const filtered = libros.filter(lib => lib.Id.includes(searchId));
-        // setFiltered(filtered);
-        // setSearchId('');
+  const handleSearch = () => {
+    // const filtered = libros.filter(lib => lib.Id.includes(searchId));
+    // setFiltered(filtered);
+    // setSearchId('');
     //     if (filtered.length === 0) {
     //       setMensaje("La consulta no arrojó datos");
     //     } else {
     //       setMensaje("");
     //     }
-      }; 
-  
+  };
 
-    
-    return (
+  return (
     <>
-       
-        <div  className=" p-10 flex items-center  rounded-lg">
-            <div style={{ backgroundColor: '#264E61' }} className="flex items-center bg-blue-600 rounded-full overflow-hidden w-full">
-                <input 
-                    type="text" 
-                    placeholder="Título, autor o género" 
-                    style={{ backgroundColor: '#264E61' }}
-                    className="p-4 text-lg w-full text-white placeholder-gray-300 bg-blue-600 focus:outline-none" 
-                />
-                <button 
-                    onClick={handleSearch} 
-                    style={{ backgroundColor: '#264E61' }}
-                    className="p-4  text-white"
-                >
-                    <FaSearch />
-                </button>
-            </div>
+      <div className="flex items-center rounded-lg p-10">
+        <div
+          style={{ backgroundColor: '#264E61' }}
+          className="flex w-full items-center overflow-hidden rounded-full bg-blue-600"
+        >
+          <input
+            type="text"
+            placeholder="Título, autor o género"
+            style={{ backgroundColor: '#264E61' }}
+            className="w-full bg-blue-600 p-4 text-lg text-white placeholder-gray-300 focus:outline-none"
+          />
+          <button
+            onClick={handleSearch}
+            style={{ backgroundColor: '#264E61' }}
+            className="p-4 text-white"
+          >
+            <FaSearch />
+          </button>
         </div>
-   
-    
-    
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
