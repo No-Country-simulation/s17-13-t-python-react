77645c644fbe1efcc2e2c451c10b8ddcdc6d9cc6
Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
2024-09-04 19:05:13 -0500

    27 feature form styles (#42)
    * feat: add changes
    
    * feat: add files
    
    * fix(frontend): :bug: fix register component
    
    * style(frontend): :bug:  refactor styles feedbackButton
    
    * style(frontend):  refactor styles feedbackButton
    
    * fix: merge conflicts
    
    * style: implement styles in components auth
    
    * fix: file pre-commit
    
    ---------
    
    Co-authored-by: wanderlee <wanderleedev@gmail.com>

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,16 +1,27 @@
 import Register from '@/components/Register';
 import Link from 'next/link';
+import '@/styles/Auth.css';
 
 export default function page() {
   return (
-    <section className="flex h-full w-full flex-col items-center gap-14 py-12">
-      <header className="flex w-full max-w-[21.875rem] flex-col items-center justify-center gap-4">
-        <h2 className="text-4xl font-black">Registrarse</h2>
-        <Link href="/login" className="pb-2 text-sm font-medium underline sm:text-lg">
-          ¿Ya tienes cuenta? Ingresar
-        </Link>
-      </header>
-      <Register />
+    <section className="background-fondo h-full">
+      <div className="m-auto flex max-w-7xl justify-end md:py-12 md:pr-20">
+        <section className="flex h-full w-full items-start justify-center bg-[#e7e0cf10] backdrop-blur-[50px] md:h-[650px] md:w-[600px] md:rounded-[35px]">
+          <div>
+            <header className="flex w-full max-w-[21.875rem] flex-col items-center justify-center gap-4">
+              <h2 className="pb-[36px] pt-[87px] text-4xl font-semibold text-[#E7E0CF]">
+                Registrarme
+              </h2>
+            </header>
+            <Register />
+            <div className="pb-10 text-center md:pb-0">
+              <Link href="/login" className="text-center text-sm text-[#E7E0CF] sm:text-base">
+                ¿Ya tienes cuenta? <b>Ingresar</b>
+              </Link>
+            </div>
+          </div>
+        </section>
+      </div>
     </section>
   );
 }
