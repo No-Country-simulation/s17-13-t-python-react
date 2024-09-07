6f1e0a01ce33ac38153df6bc3fda280f9b7f3e6c
Lucas Clemente <lucasclemente2016@gmail.com>
2024-09-06 21:19:44 -0300

    cambios de CamelCase, h2 y se quito import innecesarios

--- frontend/src/components/ReviewCard.tsx
+++ frontend/src/components/ReviewCard.tsx
@@ -1,48 +1,55 @@
 import React from 'react';
 import Image from 'next/image';
-import Star from './Star';
+import Star from './Star'; 
 import Avatar from '../../public/avatars/avatar-2.png';
 
 interface Review {
-  name: string;
-  ImageUrl?: string;
-  reviewText: string;
-  rating: number;
-  date: Date;
+    name: string;
+    imageUrl?: string;
+    reviewText: string;
+    rating: number;
+    date: Date;
 }
 
 interface ReviewCardProps {
-  review: Review;
+    review: Review;
 }
 
-const ReviewCard = ({ review }: ReviewCardProps) => {
-  return (
-    <figure className="m-4 flex flex-col items-center rounded bg-[#E7E0CF] p-4 shadow-md lg:flex-row lg:items-start">
-      <div className="m-4 flex h-auto max-w-lg flex-col items-center lg:items-start">
-        <Image src={Avatar} alt="Avatar" className="max-w-lg rounded-full sm:h-auto sm:w-32" />
-      </div>
-      <div className="flex flex-col items-center lg:flex-row lg:justify-start">
-        <div className="flex flex-col gap-1 p-4 text-center lg:text-left">
-          <figcaption className="mt-1">
-            <h2 className="font-bold">{review.name}</h2>
-            <div className="flex justify-center lg:justify-start">
-              <Star size={20} defaultRating={review.rating} color="#264E61" />
-            </div>
-          </figcaption>
-          <p
-            className={
-              'mt-2 overflow-hidden text-ellipsis text-center text-base font-bold md:text-left'
-            }
-          >
-            {review.reviewText}
-          </p>
-        </div>
-      </div>
-      <div className="mt-4 flex justify-center md:mt-0 md:justify-end">
-        <p className="font-bold">{review.date.toLocaleDateString()}</p>
-      </div>
-    </figure>
-  );
-};
+const ReviewCard = ({review} : ReviewCardProps) => {
+    return (
 
-export default ReviewCard;
+        <figure
+            className="bg-[#E7E0CF] flex flex-col lg:flex-row items-center lg:items-start m-4 p-4 rounded shadow-md">
+            <div
+                className="flex flex-col  h-auto max-w-lg items-center lg:items-start   m-4">
+                <Image
+                    src={Avatar}
+                    alt="Avatar"
+                    className="rounded-full  sm:h-auto max-w-lg     sm:w-32    "/>
+            </div>
+            <div className="flex flex-col lg:flex-row lg:justify-start items-center">
+                <div className="flex flex-col text-center lg:text-left  p-4 gap-1">
+                    <figcaption className=" mt-1">
+                        <h4 className="font-bold ">{review.name}</h4>
+                        <div className="flex justify-center lg:justify-start">
+                            <Star size={20} defaultRating={review.rating} color="#264E61"/>
+                        </div>
+                    </figcaption>
+                    <p
+                        className={`mt-2 text-ellipsis overflow-hidden text-center text-base md:text-left`}>
+                        {review.reviewText}
+                    </p>
+                </div>
+            </div>
+            <div className="flex justify-center md:justify-end  mt-4 md:mt-0">
+                <p className=" font-bold">{
+                        review .date
+                            .toLocaleDateString()
+                    }</p>
+            </div>
+        </figure>
+
+    )
+}
+
+export default ReviewCard
