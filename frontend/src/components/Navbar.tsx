32db5ee436596efa81d03282c0e372052aef41be
celinagrcs <celinagrcs@users.noreply.github.com>
2024-09-12 20:41:33 -0300

    made navbar responsive

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,11 +3,14 @@
 import React, { useRef, useState } from 'react';
 import Link from 'next/link';
 import Category from './Category';
+import { IoIosClose, IoIosMenu } from 'react-icons/io';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const stylesLink = 'cursor-pointer rounded-2xl px-3 py-2 transition-all hover:bg-[#47242A]';
 
 export default function Navbar() {
   const [showCategory, setShowCategory] = useState(false);
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
   const timerRef = useRef<number | null>(null);
 
   //////////////////////////////
@@ -23,6 +26,10 @@ export default function Navbar() {
     timerRef.current = window.setTimeout(() => setShowCategory(false), 300);
   };
 
+  const toggleMenu = () => {
+    setIsOpenMenu((prev) => !prev);
+  };
+
   //////////////////////////////
 
   return (
@@ -32,7 +39,18 @@ export default function Navbar() {
           Logo
         </Link>
 
-        <ul role="list" className="flex items-center justify-between gap-4">
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="flex items-center justify-center">
+            {isOpenMenu ? <IoIosClose className="h-[2rem] w-[1.8rem]" /> : <GiHamburgerMenu />}
+          </button>
+        </div>
+
+        <ul
+          role="list"
+          className={`hidden items-center justify-between gap-4 md:flex ${
+            isOpenMenu ? 'block' : 'hidden'
+          }`}
+        >
           <li
             onClick={() => setShowCategory((show) => !show)}
             onMouseEnter={handleMouseEnter}
@@ -72,6 +90,44 @@ export default function Navbar() {
           </li>
         </ul>
       </nav>
+
+      {isOpenMenu && (
+        <ul className="flex flex-col items-center gap-4 bg-[#62262E] py-4 md:hidden">
+          <li onClick={() => setShowCategory((show) => !show)}>
+            <a className={stylesLink}>Categoría</a>
+          </li>
+
+          <li>
+            <Link href="/allBooks" className={stylesLink}>
+              Todos los libros
+            </Link>
+          </li>
+
+          <li>
+            <a href="/#TopTen" className={stylesLink}>
+              Top ten
+            </a>
+          </li>
+
+          <li>
+            <a href="/#recentlyAdded" className={stylesLink}>
+              Agregados recientemente
+            </a>
+          </li>
+
+          <li>
+            <Link href="/myFavorites" className={stylesLink}>
+              Mis favoritos
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/login" className={stylesLink}>
+              Iniciar sesión
+            </Link>
+          </li>
+        </ul>
+      )}
 
       {showCategory && (
         <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
